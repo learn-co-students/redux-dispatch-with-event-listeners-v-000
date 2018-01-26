d4e8979471e80770aca58a75d9cbbb545a42fa9e
github username <github email address>
2018-01-26 14:50:06 -0800

    Done.

--- reducer.js
+++ reducer.js
@@ -7,3 +7,22 @@ function changeState(state = {count: 0}, action){
         return state;
     }
   }
+
+  function dispatch(action){
+    state = changeState(state, action);
+    render ();
+    }
+
+
+  function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  }
+
+  let button = document.getElementById('button');
+ 
+button.addEventListener('click', function() {
+  dispatch({ type: 'INCREASE_COUNT' });
+});
+
+  dispatch({ type: '@@INIT' })
