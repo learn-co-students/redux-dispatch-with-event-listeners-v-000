59c2ff2aca6dae50da8b316e546e08c7cfcd068c
CLackrone <charisaanne@gmail.com>
2018-03-20 11:05:44 -0400

    Done.

--- reducer.js
+++ reducer.js
@@ -1,9 +1,29 @@
-let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
-  }
+let state 
+  
+const changeState = (state = {count: 0}, action) => {
+  switch (action.type) {
+       case 'INCREASE_COUNT':
+       return {count: state.count + 1};
+ 
+     default:
+       return state;
+   }
+}
+
+const dispatch = action => {
+  state = changeState(state, action);
+  render();
+}
+
+const render = () => {
+  let container = document.getElementById('container');
+  container.textContent = state.count
+}
+
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+dispatch({ type: '@@INIT' })
