344ffdb3252c7e78307158d81a62ff04aae293cc
Christian Egglin <cegglin@conncoll.edu>
2018-04-06 19:05:42 +0000

    Done.

--- reducer.js
+++ reducer.js
@@ -1,9 +1,27 @@
 let state;
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function () {
+  dispatch({type: 'INCREASE_COUNT'});
+});
+
 function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
   }
+}
+
+function dispatch (action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render () {
+  document.getElementById('container').textContent = state.count;
+}
+
+dispatch({type: '@@INIT'})
