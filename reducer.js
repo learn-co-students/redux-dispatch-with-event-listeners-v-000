8bc4f859a91e02da08a5fe1c32b815b8b1cfca1a
Gingertonic <bethmschofield@gmail.com>
2018-10-08 20:34:43 +0100

    Done.

--- reducer.js
+++ reducer.js
@@ -1,1 +1,36 @@
 // add code snippets from README
+let state;
+let button = document.getElementById('button')
+let downButton = document.getElementById('down_button')
+
+button.addEventListener('click', function(){
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+downButton.addEventListener('click', function(){
+  dispatch({type: 'DECREASE_COUNT'})
+})
+
+function reducer(state = { count: 0 }, action){
+  switch(action.type){
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+    case 'DECREASE_COUNT':
+      return { count: state.count - 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+
+dispatch({type: '@@INIT'})
