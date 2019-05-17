c5ab1575f8e58beb7745908c96897dc5946ce7c7
github username <Adeel.A.Mhd@gmail.com>
2019-05-17 19:26:08 -0400

    Done.

--- reducer.js
+++ reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+function render() {
+  document.querySelector('#container').innerHTML = state.count
+}
+
+function handleClick() {
+  document.getElementById('button').addEventListener('click', () => dispatch({ type: 'INCREASE_COUNT' }))
+}
+
+dispatch({ type: '@@INIT' })
+
+handleClick()
