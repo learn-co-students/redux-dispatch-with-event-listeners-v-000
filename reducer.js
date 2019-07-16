8842ea56a45e33c9d1015a9072682263e6f98eeb
Jamie Giuliano <jamiepgiuliano@gmail.com>
2019-07-16 10:33:54 -0700

    Done.

--- reducer.js
+++ reducer.js
@@ -1,1 +1,42 @@
-// add code snippets from README
+let store = createStore(changeCount) // createStore takes the changeCount reducer as an argument
+let button = document.getElementById('button');
+
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
+
+function changeCount(state = { count: 0 }, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+
+    default:
+      return state;
+  }
+}
+
+
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count;
+};
+
+store.dispatch({ type: '@@INIT' });
+
+button.addEventListener('click', function() {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
