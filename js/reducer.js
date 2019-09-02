7253a1d9b1467d4f483e090e95add3a795a95c1c
Christopher DiStasio <cadistasio@gmail.com>
2019-09-02 14:45:50 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,22 @@
-let state;
+function createStore(reducer){
+  let state;
 
-function reducer(state = {count: 0}, action){
+  function dispatch(action){
+    state = changeCount(state, action);
+    render()
+  }
+
+  function getState() {
+    return state;
+  }
+ 
+  return {
+    dispatch,
+    getState
+  };
+}
+
+function changeCount(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
@@ -9,20 +25,17 @@ function reducer(state = {count: 0}, action){
   }
 }
 
-function dispatch(action){
-  state = reducer(state, action);
-  render()
-}
-
 function render(){
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({type: '@@INIT'})
+let store = createStore(changeCount);
+
+store.dispatch({ type: '@@INIT' });
 
 let button = document.getElementById('button');
  
 button.addEventListener('click', function(){
-  dispatch({type: 'INCREASE_COUNT'})
+  store.dispatch({ type: 'INCREASE_COUNT' });
 })
