cf1e51fb6bf7ebe8e438d067f9f575a240cefdfb
Rafael Lopez <lpz.raf@gmail.com>
2019-09-16 20:16:42 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,17 @@
 // add code snippets from README
+let state = {count: 0}
+ 
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+reducer({count: 0}, {type: 'INCREASE_COUNT'});
+
+function dispatch(action){
+    state = reducer(state, action);
+  }
