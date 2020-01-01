f7944d09e492629a42714a7acaac07ff5ce36cfc
Nate Dietz <nsdietz@gmail.com>
2020-01-01 14:47:06 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,26 @@
-// add code snippets from README
+let state;
+ 
+function reducer(state= {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function render() {
+	const container = document.getElementById('container')
+	container.textContent = state.count
+}
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => this.dispatch({type: 'INCREASE_COUNT'}))
+
+function dispatch(action) {
+	state = reducer(state, action)
+	render()
+}
+
+dispatch({type: '@@INIT'})
