7a4f0fa086bebc392cbb0a9d8692a13f59ca8064
Shaun McLoughlin <skmcloughlin@gmail.com>
2020-05-27 07:01:41 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,38 @@
-// add code snippets from README
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+Copy this and the previous snippet into
+
+Now every time we click, we dispatch an action of type increase. Dispatch first calls our reducer, which updates our state. Then dispatch renders the updated view.
+
+Putting everything together, our code should look like this:
+
+let state;
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
+}
+ 
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+}
+ 
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+ 
+dispatch({type: '@@INIT'})
+ 
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
