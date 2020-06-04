e16da5f19db53f0cac0138a38a2365da0ba69dc6
leothebeagle <a.kosber@gmail.com>
2020-06-04 13:21:19 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
 // add code snippets from README
+let state;
+let button = document.getElementById('button');
+
+function reducer (state = {count:0}, action) {
+    switch (action.type) {
+        case 'INCREASE_COUNT':
+            return {count: state.count + 1};
+        default:
+            return state;
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action);
+    render();
+}
+// the dispatch function, will call the changeState function
+// and then it'll change the wire that the current state is connected to
+// it'll direct that wire to connect to the result of the changeSate function.
+
+function render() {
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+button.addEventListener('click', () => {
+    dispatch({type:'INCREASE_COUNT'})
+})
+
+dispatch({type: '@@INIT'})
