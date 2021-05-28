ca576372369353078b3c264e046ecd26389615a7
guysbryant <guy.s.bryant@gmail.com>
2021-05-27 22:16:08 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,20 @@
 // add code snippets from README
+let state
+
+const reducer = (state = {count: 0}, action) => {
+    switch(action.type){
+    case "INCREASE_COUNT":
+        return {count: state.count + 1}
+    default: return state
+    }
+}
+
+let action = (type) => ({type})
+const increaseCountButton = document.getElementById("button")
+increaseCountButton.addEventListener("click", () => dispatch(action("INCREASE_COUNT")))
+const dispatch = (action) => {state = reducer(state, action); render()}
+const container = document.getElementById("container")
+const render = () => container.textContent = state.count
+
+dispatch(action("@@INIT"))
+
